351f9e10f13606d32a067234202aeeb780f267b4
LiXiong <li.xiong.terance@gmail.com>
2018-10-31 00:15:45 +0800

    index page code complete

---
+++ apps/work_notes/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # homepage
+    url(r'^$', views.index, name='index'),
+    # list dirs and notes
+    #url(r'^notes/dir1/dir2/note1$', views.notes, name='notes'),
+    ## add dir
+    #url(r'^new_dir/$', views.new_dir, name='new_dir'),
+    ## add note
+    #url(r'^new_note/dir1/$', views.new_note, name='new_note'),
+    ## modify item
+    #url(r'^edit/id/$', views.edit, name='edit'),
+    ## delete item
+    #url(r'^delete/id/$', views.delete, name='delete'),
+]
